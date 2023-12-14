81678a72a22cf7799ddb5695aa4f37838c3a49c3
asandan <assanali.danybayev@gmail.com>
2023-12-14 15:36:47 +0600

    Remove unnecessary comments

--- util/extractNumber.ts
+++ util/extractNumber.ts
@@ -1,8 +1,6 @@
 export const extractNumber = (inputString: string) => {
-  // Use a regular expression to extract the number
   const match = inputString.match(/\d+/);
 
-  // Check if a match was found
   if (match) {
     return parseInt(match[0], 10);
   }
