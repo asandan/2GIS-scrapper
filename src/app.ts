3855ec50f39a5ebc7b7a9a86e9c1e61ae00612b4
asandan <assanali.danybayev@gmail.com>
2023-12-15 15:21:01 +0600

    Changed format of review scrapping result. Now saving results locally in reviews.json

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,14 @@
 import OpenAI from "openai";
 import puppeteer from "puppeteer";
 import { extractNumber } from "../util/extractNumber";
+import fs from "fs";
+
+export type Reviews = {
+  reviews: {
+    reviewerName: string | undefined;
+    review: string | undefined;
+  }[];
+};
 
 const openai = new OpenAI({
   apiKey: "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",
@@ -34,14 +42,12 @@ const scrape2GISPage = async (url: string) => {
 
   await page.goto(url);
 
-  const data = [];
+  const data: Reviews = {
+    reviews: [],
+  };
 
   const reviews = await page.evaluate(() => {
-    const element = document.querySelectorAll("._r47nf");
-    const totalReviews = element![1]
-      .querySelector("._jspzdm")
-      ?.textContent?.trim();
-    return totalReviews;
+    return document.querySelector("._1xhlznaa")?.textContent?.trim();
   });
 
   const totalReviews = extractNumber(reviews!)!;
@@ -53,19 +59,32 @@ const scrape2GISPage = async (url: string) => {
     await page.click("._1iczexgz");
 
     await new Promise((resolve) => setTimeout(resolve, 500));
+    
     console.log(`${Math.floor((i / pagesToScrape) * 100)}% has been parsed`);
 
-    const reviews = await page.$$eval("._49x36f", (elements) =>
-      elements.map((e) => e.textContent?.trim())
+    const reviews = await page.$$eval("._11gvyqv", (elements) =>
+      elements.map((e) => {
+        const reviewerName = e.querySelector("._er2xx9")?.textContent?.trim();
+        const review = e.querySelector("._49x36f")?.textContent?.trim();
+
+        return {
+          reviewerName,
+          review,
+        };
+      })
     );
 
+    // console.log(reviews)
     const newReviews = reviews.slice(i * 12);
 
-    data.push(...newReviews);
+    data.reviews.push(...newReviews);
   }
-  console.log(data, data.length);
+  console.log(data.reviews, data.reviews.length);
+
+  fs.writeFileSync("reviews.json", JSON.stringify(data));
 
   await browser.close();
 };
 
 scrape2GISPage(url);
+
